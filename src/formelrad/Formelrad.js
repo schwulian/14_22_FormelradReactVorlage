5619dbbce16d1d36033aa8e7733601eb2bc90255
jun <Julian.Nagel@ti8m.ch>
2025-06-15 20:21:44 +0200

    0021-0022

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -11,6 +11,12 @@ export default function Formelrad() {
         p: ""
     })
 
+    const handleClear = (event) => {
+        event.preventDefault();
+        console.log("handleClear");
+        setValues(values => ({...values, u:"", i:"", r:"", p:"", message: ""}))
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("handleSubmit")
@@ -54,6 +60,7 @@ export default function Formelrad() {
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
+                    <button style={{margin:10}} onClick={handleClear}>Clear</button>
                 </form>
             </section>
         </>
